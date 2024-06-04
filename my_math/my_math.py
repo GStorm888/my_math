55951cd318656741f1020a0ae3003d3d2c2e64a3
GStorm888 <denchikevstr@icloud.com>
2024-06-04 22:49:30 +0300

    написал функцию average

--- my_math/my_math.py
+++ my_math/my_math.py
@@ -11,4 +11,17 @@ def min():
         min = second_num
     else:
         min = first_num
-        
+
+def average():
+    first_num = input("input first number")
+    second_num = input("input second number")
+
+    if not first_num.isdigit() and not second_num.isdigit():
+        return "please enter a number"
+    
+    if first_num < second_num:
+        average = second_num - first_num
+    elif first_num > second_num:
+        average =  first_num - second_num
+    else:
+        average = 0
