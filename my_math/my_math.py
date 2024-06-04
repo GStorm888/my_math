a7ec79a4ff4f8a257039b833fb2a632036936a13
GStorm888 <denchikevstr@icloud.com>
2024-06-04 22:39:52 +0300

    вынес метод isdigit в отдельную функцию

--- my_math/my_math.py
+++ my_math/my_math.py
@@ -1,8 +1,14 @@
-def min():
-    first_num = input("input first number")
-    second_num = input("input second number")
+def is_digit(first_num, second_num):
+    if first_num.isdigit() and second_num.isdigit():
+        return False
+    else:
+        return  True
 
-    if not first_num.isdigit() and not second_num.isdigit():
+def min_func():
+    first_num = input("input first number >>>")
+    second_num = input("input second number >>>")
+
+    if is_digit(first_num, second_num):
         return "please enter a number"
     
     if first_num < second_num:
@@ -11,4 +17,4 @@ def min():
         min = first_num
     else:
         min = first_num
-        
+    return min
